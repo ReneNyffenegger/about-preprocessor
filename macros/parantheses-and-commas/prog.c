1adc8d9b9248cd8bd283765775d3d6b98f82db9c
René Nyffenegger <mail@renenyffenegger.ch>
2025-08-22 08:07:31 +0200

    add cond parameter to macro

--- macros/parantheses-and-commas/prog.c
+++ macros/parantheses-and-commas/prog.c
@@ -1,6 +1,6 @@
-#define call(func, arg) func(arg);
+#define call(func, arg, cond) if (cond) { func(arg); }
 #define COMMA ,
 
-call(printf, "foo bar baz\n")
-call(printf, "a = %d, b=%s\n" COMMA a COMMA b)
-call(printf, "d = %d, s=%s\n" COMMA (a+b)*2 COMMA get_val(1 COMMA 2) )
+call(printf, "foo bar baz\n", true)
+call(printf, "a = %d, b=%s\n" COMMA a COMMA b, get_flag("a" COMMA "b"))
+call(printf, "d = %d, s=%s\n" COMMA (a+b)*2 COMMA get_val(1 COMMA 2), get_flag("a" COMMA "b") )
